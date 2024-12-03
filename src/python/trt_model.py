cce2ec94b6b0e30c7c44b18a56c796249d94e3b5
HarryCaveMan <harris.joseph.088@gmail.com>
2024-12-03 01:45:43 -0500

    lots of updates

--- src/python/trt_model.py
+++ src/python/trt_model.py
@@ -5,8 +5,10 @@ import pycuda.driver as cuda
 import pycuda.autoinit
 import tensorrt as trt
 import asyncio
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 from itertools import cycle
+from copy import deepcopy
 
 class HostDeviceMem:
     def __init__(self, host_mem, device_mem, shape):
@@ -15,35 +17,36 @@ class HostDeviceMem:
         self.device = device_mem
         # keeping track of shape to un-flatten it later
         self.shape = shape
+        
 
+class IOBufferSet:
+    def __init__(self,inputs,outputs,bindings,stream,thread_pool) -> None:
+        self._thread_pool = thread_pool
+        self._loop = asyncio.get_running_loop()
+        self._input_guard = asyncio.Lock()
+        self._output_guard = asyncio.Lock()
+        self.inputs = inputs
+        self.output = outputs
+        self.bindings = bindings
+        self.stream = stream
+        self._free = True
 
-class TRTContextWithStreamAndBuffers:
-    def allocate_buffers(self):
-    """Allocate buffers dynamically based on the engine's context and current input shape."""
-    inputs, outputs, bindings = [], [], []
-    stream = cuda.Stream()
-    for binding_index in range(self._trt_context.engine.num_bindings):
-        binding_name = self._trt_context.engine.get_binding_name(binding_index)
-        shape = self._trt_context.get_binding_shape(binding_name)
-        size = trt.volume(shape)
-        dtype = trt.nptype(self._trt_context.engine.get_binding_dtype(binding_name))
-        host_mem = cuda.pagelocked_empty(size, dtype)
-        device_mem = cuda.mem_alloc(host_mem.nbytes)
-        bindings.append(int(device_mem))
-        mem_obj = HostDeviceMem(host_mem, device_mem, shape)
-        if self._trt_context.engine.binding_is_input(binding_name):
-            inputs.append(mem_obj)
-        else:
-            outputs.append(mem_obj)
-    return inputs, outputs, bindings, stream
+    def taint(self) -> None:
+        self._free = False
 
-    async def attach(self) -> None:
-        await self._lock.acquire()
-        self._in_use = True
+    def release(self) -> None:
+        self._free = True
 
-    def detach(self) -> None:
-        self._lock.release()
-        self._in_use = False
+    @property
+    def idle(self) -> bool:
+        return self._free
+
+    async def sync_cuda_event(self,event) -> None:
+        await self._loop.run_in_executor(
+            self._thread_pool,
+            self.stream.wait_for_event,
+            event
+        )
 
     def dtoh_async(self) -> None:
         for output in self.outputs:
@@ -53,88 +56,110 @@ class TRTContextWithStreamAndBuffers:
                 self.stream
             )
 
-    def push_async(self,input_data,input_index=0) -> None:
-            np.copyto(_input.host, input_data.ravel())
-            cuda.memcpy_htod_async(
-                self.inputs[input_index].device, 
-                self.inputs[input_index].host, 
-                self.stream
-            )
+    def htod_async(self,input_index) -> None:
+        cuda.memcpy_htod_async(
+            self.inputs[input_index].device, 
+            self.inputs[input_index].host, 
+            self.stream
+        )
+
+    async def push_one(self,input_data,input_index=0) -> None:
+        completion_event = cuda.Event()
+        await self._input_guard.acquire()
+        try:
+            np.copyto(self.inputs[input_index].host, input_data.ravel())
+            self.htod_async(input_index)
+            self.stream.record(completion_event)
+            await self.sync_cuda_event(completion_event)
+        finally:
+            self._input_guard.release()
+
+    async def begin_read(self) -> None:
+        await self._output_guard.acquire()
 
     async def pull(self,event) -> np.ndarray:
-        await self._loop.run_in_executor(
-            None,
-            self.stream.wait_for_event,
-            event
-        )
+        await self.sync_cuda_event(event)
         outputs = []
-        for host_device_output in self.outputs:
-            output = host_device_output.host  # This is a NumPy ndarray
-            output = output.reshape(host_device_output.shape)  # Reshape to original tensor shape
-            outputs.append(output)
-        return outputs
+        try:
+            for host_device_output in self.outputs:
+                output = deepcopy(host_device_output.host)  # This is a NumPy ndarray
+                output = output.reshape(host_device_output.shape)  # Reshape to original tensor shape
+                outputs.append(output)            
+            return outputs
+        finally:
+            self._output_guard.release() # Can free output Buffer for reuse after deepcopy
+    
 
+class TRTContextWithStreamAndBuffers:
+    def allocate_buffers(self):
+        inputs,outputs,bindings = [],[],[]
+        for binding_index in range(self._trt_context.engine.num_bindings):
+            binding_name = self._trt_context.engine.get_binding_name(binding_index)
+            shape = self._trt_context.get_binding_shape(binding_name)
+            size = trt.volume(shape)
+            dtype = trt.nptype(self._trt_context.engine.get_binding_dtype(binding_name))
+            host_mem = cuda.pagelocked_empty(size, dtype)
+            device_mem = cuda.mem_alloc(host_mem.nbytes)
+            bindings.append(int(device_mem))
+            mem_obj = HostDeviceMem(host_mem, device_mem, shape)
+            if self._trt_context.engine.binding_is_input(binding_name):
+                inputs.append(mem_obj)
+            else:
+                outputs.append(mem_obj)
+        return IOBufferSet(inputs,outputs,bindings,self.stream,self._io_thread_pool)
+
+    def get_io_buffers(self) -> IOBufferSet:
+        return next(
+            # Try for next available
+            (buffer_set for buffer_set in self._buffer_pool if buffer_set.idle),
+            # default to round-robin if none are idle (The buffers have locking anyway)
+            next(cycle(self._buffer_pool))
+        )
+    
     async def execute(self,input_data) -> np.ndarray:
         completion_event = cuda.Event()
-        self.push_async(input_data)
-        self._trt_context.execute_async_v3(self.stream.handle)
-        self.dtoh_async()
+        buffers = self.get_io_buffers()
+        await buffers.push_one(input_data)
+        self._trt_context.enqueue_v3(buffers.stream.handle,buffers.bindings)
+        await buffers.begin_read()
+        buffers.dtoh_async()
         completion_event.record(self.stream)
-        return await self.pull(completion_event)
+        return await buffers.pull(completion_event)
   
     @property
-    def available(self):
-        return not self._in_use
+    def idle(self):
+        return not self.stream.is_done()
 
-    def __init__(self,engine) -> None:
-        self._loop = asyncio.get_running_loop()
-        self._lock = asyncio.Lock()
+    def __init__(self,engine,num_buffers) -> None:
+        self._io_thread_pool = ThreadPoolExecutor(max_workers=num_buffers*2)
+        self._input_guard = asyncio.Lock()
+        self._output_guard = asyncio.Lock()
         self._trt_context = engine.create_execution_context()
-        self.inputs,self.outputs,self.bindings,self.stream = self.allocate_buffers(engine)
-        self.detach()
+        self.stream = cuda.Stream()
+        self._buffer_pool = tuple((self.allocate_buffers() for i in range(num_buffers)))
 
 
 class TRTExecPool:
-    def __init__(self,engine,max_concurrency,logger) -> None:
-        self._guard = asyncio.Semaphore(max_concurrency)
+    def __init__(self,engine,num_cuda_streams,num_buffers_per_stream) -> None:
+        self._guard = asyncio.Semaphore(num_cuda_streams)
         self._execution_contexts = (
-            TRTContextWithStreamAndBuffers(engine)
-            for i in range(max_concurrency)
+            TRTContextWithStreamAndBuffers(engine,num_buffers_per_stream)
+            for i in range(num_cuda_streams)
         )
     
-    async def get_execution_context(self):
-        await self._guard.acquire()
-        try:
-            context = next(
-                # Try for next available
-                filter(
-                    lambda stream:stream.available,
-                    self._execution_contexts
-                ),
-                # default to round-robin if none available
-                # await below will still wait until the context lock is released
-               next(cycle(self._execution_contexts))
-            )
-            await context.attach()
-            return context
-        except Exception as e:
-            try: context.detach()
-            # just pass if exceptioin was thrown before stream attach
-            except Exception: pass
-            self._guard.release()
-            raise e
-
-    def release_stream(self,stream):
-        try:
-            stream.detach()
-        finally:
-            self._guard.release()
+    def get_execution_context(self):
+        return next(
+            # Try for next available
+            (stream for stream in self._execution_contexts if stream.idle),
+            # default to round-robin if none are idle
+            next(cycle(self._execution_contexts))
+        )
 
 
 class TRTModel:
     def load_engine(self, model_path) -> None:
         # loads the model from given filepath
-        with open(Path(engine_path)/"model.trt", "rb") as f:
+        with open(Path(model_path)/"model.trt", "rb") as f:
             self._trt_engine = self._runtime.deserialize_cuda_engine(f.read())
 
     def load_tokenizer(self,model_path) -> None:
@@ -142,20 +167,19 @@ class TRTModel:
         self._hf_config = AutoConfig.from_pretrained(model_path)
         self._max_seq_len = self._hf_config.max_position_embeddings
 
-    def __init__(self,model_path,max_concurrency=1) -> None:
+    def __init__(self,model_path,num_cuda_streams=1,io_buffer_sets_per_stream=4) -> None:
         self._logger = trt.Logger(trt.Logger.ERROR)
-        self._runtime = trt.Runtime(self.logger)
+        self._runtime = trt.Runtime(self._logger)
         self.load_engine(model_path)
         self.load_tokenizer(model_path)
-        self._exec_pool = TRTExecPool(self._trt_engine,max_concurrency)
+        self._exec_pool = TRTExecPool(self._trt_engine,num_cuda_streams,io_buffer_sets_per_stream)
 
     async def predict(self,input_data):
-        executor = await self._exec_pool.get_execution_context()
-        inputs_np = self._tokenizer.tokenize(
+        executor = self._exec_pool.get_execution_context()
+        inputs_np = self._tokenizer(
             input_data,
             truncation=True,
             max_length=self._max_seq_len,
-            output_tensors="np"
+            return_tensors="np"
         )
-
-        
+        return await executor.execute(inputs_np["input_ids"])
