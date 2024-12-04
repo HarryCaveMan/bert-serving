f84364e75a2043cc14a7d393d279a7c6a011916b
HarryCaveMan <harris.joseph.088@gmail.com>
2024-12-04 17:27:07 -0500

    putting cuda io in its on module

---
+++ src/python/cuda_asyncio.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pycuda.driver as cuda
+import pycuda.autoinit
+import tensorrt as trt
+import asyncio
+
+class HostDeviceMem:
+    def __init__(self, host_mem, device_mem, shape):
+        # keeping track of addresses
+        self.host = host_mem
+        self.device = device_mem
+        # keeping track of shape to un-flatten it later
+        self.shape = shape
+
+
+class IOBufferSet:
+    def __init__(self,inputs,outputs,bindings,stream,thread_pool) -> None:
+        self._thread_pool = thread_pool
+        self._loop = asyncio.get_running_loop()
+        self._input_guard = asyncio.Lock()
+        self._output_guard = asyncio.Lock()
+        self.inputs = inputs
+        self.output = outputs
+        self.bindings = bindings
+        self.stream = stream
+        self._taints = 0
+
+    def taint(self) -> None:
+        self._taints += 1
+
+    def clean(self) -> None:
+        self._taints -= 1
+
+    @property
+    def taints(self) -> int:
+        return self._taints
+
+    @property
+    def idle(self) -> bool:
+        return self._taints == 0
+        
+    # Awaits a cuda stream event asynchronously without blocking the main event loop
+    # Currently each buffer gets its own thread because The streams I/O buffers lock independently of each other
+    # CUDA streams handle synchronization of the accelerator, so locks only needed for HOST buffers
+    # Buffers only lock when dependent on the HOST (output)buffer contents or while mutating the HOST (input)buffer
+    async def sync_cuda_event(self,event) -> None:
+        await self._loop.run_in_executor(
+            self._thread_pool,
+            self.stream.wait_for_event,
+            event
+        )
+
+    def dtoh_async(self) -> None:
+        for output in self.outputs:
+            cuda.memcpy_dtoh_async(
+                output.host, 
+                output.device, 
+                self.stream
+            )
+
+    def htod_async(self,input_index) -> None:
+        cuda.memcpy_htod_async(
+            self.inputs[input_index].device, 
+            self.inputs[input_index].host, 
+            self.stream
+        )
+
+    async def push_one(self,input_data,input_index=0) -> None:
+        htod_event = cuda.Event()
+        async with self._input_guard:
+            np.copyto(self.inputs[input_index].host, input_data.ravel())
+            self.htod_async(input_index)
+            self.stream.record(write_event)
+            await self.sync_cuda_event(htod_event)
+
+    async def pull(self,event) -> np.ndarray:
+        async with self._output_guard:
+            self.dtoh_async()
+            event.record(self.stream)
+            await self.sync_cuda_event(event)
+            outputs = []
+            for host_device_output in self.outputs:
+                output = np.copy(host_device_output.host)  # This is a NumPy ndarray
+                output = output.reshape(host_device_output.shape)  # Reshape to original tensor shape
+                outputs.append(output)            
+            return outputs
--- src/python/trt_model.py
+++ src/python/trt_model.py
@@ -1,99 +1,24 @@
 from transformers import AutoConfig,AutoTokenizer
-import tensorrt as trt
 import numpy as np
-import pycuda.driver as cuda
-import pycuda.autoinit
 import tensorrt as trt
-import asyncio
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
-from itertools import cycle
+import asyncio
 
-class HostDeviceMem:
-    def __init__(self, host_mem, device_mem, shape):
-        # keeping track of addresses
-        self.host = host_mem
-        self.device = device_mem
-        # keeping track of shape to un-flatten it later
-        self.shape = shape
-
-
-class IOBufferSet:
-    def __init__(self,inputs,outputs,bindings,stream,thread_pool) -> None:
-        self._thread_pool = thread_pool
-        self._loop = asyncio.get_running_loop()
-        self._input_guard = asyncio.Lock()
-        self._output_guard = asyncio.Lock()
-        self.inputs = inputs
-        self.output = outputs
-        self.bindings = bindings
-        self.stream = stream
-        self._taints = 0
-
-    def taint(self) -> None:
-        self._taints += 1
-
-    def clean(self) -> None:
-        self._taints -= 1
-
-    @property
-    def idle(self) -> bool:
-        return self._taints == 0
-        
-    # Awaits a cuda stream event asynchronously without blocking the main event loop
-    # Currently each buffer gets its own thread because The streams I/O buffers lock independently of each other
-    # BUffers only lock when dependent on the HOST (output)buffer contents or are mutating the HOST (input)buffer 
-    async def sync_cuda_event(self,event) -> None:
-        await self._loop.run_in_executor(
-            self._thread_pool,
-            self.stream.wait_for_event,
-            event
-        )
-
-    def dtoh_async(self) -> None:
-        await buffers.begin_read()
-        for output in self.outputs:
-            cuda.memcpy_dtoh_async(
-                output.host, 
-                output.device, 
-                self.stream
-            )
-
-    def htod_async(self,input_index) -> None:
-        cuda.memcpy_htod_async(
-            self.inputs[input_index].device, 
-            self.inputs[input_index].host, 
-            self.stream
-        )
-
-    async def push_one(self,input_data,input_index=0) -> None:
-        completion_event = cuda.Event()
-        await self._input_guard.acquire()
-        try:
-            np.copyto(self.inputs[input_index].host, input_data.ravel())
-            self.htod_async(input_index)
-            self.stream.record(completion_event)
-            await self.sync_cuda_event(completion_event)
-        finally:
-            self._input_guard.release()        
-
-    async def pull(self,event) -> np.ndarray:
-        await self._output_guard.acquire()
-        self.dtoh_async()
-        event.record(self.stream)
-        await self.sync_cuda_event(event)
-        outputs = []
-        try:
-            for host_device_output in self.outputs:
-                output = np.copy(host_device_output.host)  # This is a NumPy ndarray
-                output = output.reshape(host_device_output.shape)  # Reshape to original tensor shape
-                outputs.append(output)            
-            return outputs
-        finally:
-            self._output_guard.release() # Can free output Buffer for reuse after deepcopy
-    
+from cuda_asyncio import HostDeviceMem,IOBufferSet
 
 class TRTContextWithStreamAndBuffers:
+    def __init__(self,engine,num_buffer_sets) -> None:
+        # Multiply by two because each buffer set contains:
+        #   - one set of input buffers
+        #   - one set of output buffers
+        self._io_thread_pool = ThreadPoolExecutor(max_workers=num_buffer_sets*2)
+        self._input_guard = asyncio.Lock()
+        self._output_guard = asyncio.Lock()
+        self._trt_context = engine.create_execution_context()
+        self.stream = cuda.Stream()
+        self._buffer_pool = tuple(self.allocate_buffers() for i in range(num_buffer_sets))
+
     def allocate_buffers(self):
         inputs,outputs,bindings = [],[],[]
         for binding_index in range(self._trt_context.engine.num_bindings):
@@ -115,8 +40,8 @@ class TRTContextWithStreamAndBuffers:
         return next(
             # Try for next available
             (buffer_set for buffer_set in self._buffer_pool if buffer_set.idle),
-            # default to round-robin if none are idle (The buffers have locking anyway)
-            next(cycle(self._buffer_pool))
+            # default to least busy (fewest taints) if none are idle (The buffers have locking anyway)
+            min(self._buffer_pool, key=lambda buffer_set: buffer_set.taints)
         )
     
     async def execute(self,input_data) -> np.ndarray:
@@ -125,8 +50,8 @@ class TRTContextWithStreamAndBuffers:
         buffers.taint()
         await buffers.push_one(input_data)
         self._trt_context.enqueue_v3(
-            buffers.stream.handle,
-            buffers.bindings
+            buffers.bindings,
+            buffers.stream.handle
         )
         model_output = await buffers.pull(completion_event)
         buffers.clean()
@@ -136,22 +61,17 @@ class TRTContextWithStreamAndBuffers:
     def idle(self):
         return not self.stream.is_done()
 
-    def __init__(self,engine,num_buffers) -> None:
-        self._io_thread_pool = ThreadPoolExecutor(max_workers=num_buffers*2)
-        self._input_guard = asyncio.Lock()
-        self._output_guard = asyncio.Lock()
-        self._trt_context = engine.create_execution_context()
-        self.stream = cuda.Stream()
-        self._buffer_pool = tuple((self.allocate_buffers() for i in range(num_buffers)))
+    @property
+    def taints(self) -> int:
+        return sum(buffer_set.taints for buffer_set in self._buffer_pool)
 
 
 class TRTExecPool:
-    def __init__(self,engine,num_cuda_streams,num_buffers_per_stream) -> None:
-        self._guard = asyncio.Semaphore(num_cuda_streams)
-        self._execution_contexts = (
+    def __init__(self,engine,num_cuda_streams,io_buffer_sets_per_stream) -> None:
+        self._execution_contexts = tuple(
             TRTContextWithStreamAndBuffers(
                 engine,
-                num_buffers_per_stream
+                io_buffer_sets_per_stream
             )
             for i in range(num_cuda_streams)
         )
@@ -160,8 +80,8 @@ class TRTExecPool:
         return next(
             # Try for next available
             (stream for stream in self._execution_contexts if stream.idle),
-            # default to round-robin if none are idle
-            next(cycle(self._execution_contexts))
+            # default to least busy (fewest taints) if none are idle
+            min(self._execution_contexts, key=lambda context: context.taints)
         )
 
 
